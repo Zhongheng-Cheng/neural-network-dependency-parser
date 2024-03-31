79d82eddddddd8d6b51fb60bf88b173e24f5a28d
Zhongheng-Cheng <zc2737@columbia.edu>
2024-03-31 14:08:39 -0400

    adjusted indentation

--- train_model.py
+++ train_model.py
@@ -10,108 +10,107 @@ from extract_training_data import FeatureExtractor
 
 class DependencyDataset(Dataset):
 
-  def __init__(self, input_filename, output_filename):
-    self.inputs = np.load(input_filename)
-    self.outputs = np.load(output_filename)
+	def __init__(self, input_filename, output_filename):
+		self.inputs = np.load(input_filename)
+		self.outputs = np.load(output_filename)
 
-  def __len__(self): 
-    return self.inputs.shape[0]
+	def __len__(self): 
+		return self.inputs.shape[0]
 
-  def __getitem__(self, k): 
-    return (self.inputs[k], self.outputs[k])
+	def __getitem__(self, k): 
+		return (self.inputs[k], self.outputs[k])
 
 
 class DependencyModel(Module): 
 
-  def __init__(self, word_types, outputs):
-    super(DependencyModel, self).__init__()
-    self.embedding = Embedding(num_embeddings=word_types, embedding_dim=128)
-    self.hidden_layer = Linear(128 * 6, 128)
-    self.output_layer = Linear(128, 91)
+	def __init__(self, word_types, outputs):
+		super(DependencyModel, self).__init__()
+		self.embedding = Embedding(num_embeddings=word_types, embedding_dim=128)
+		self.hidden_layer = Linear(128 * 6, 128)
+		self.output_layer = Linear(128, 91)
 
-  def forward(self, inputs):
-    embedded = self.embedding(inputs)
-    embedded = embedded.view(torch.Size([embedded.size(0), embedded.size(1) * embedded.size(2)]))
-    hidden_out = self.hidden_layer(embedded)
-    hidden_out = relu(hidden_out)
-    out = self.output_layer(hidden_out)
-    return out
+	def forward(self, inputs):
+		embedded = self.embedding(inputs)
+		embedded = embedded.view(torch.Size([embedded.size(0), embedded.size(1) * embedded.size(2)]))
+		hidden_out = self.hidden_layer(embedded)
+		hidden_out = relu(hidden_out)
+		out = self.output_layer(hidden_out)
+		return out
 
 
 def train(model, loader): 
 
-  loss_function = CrossEntropyLoss(reduction='mean')
+    loss_function = CrossEntropyLoss(reduction='mean')
 
-  LEARNING_RATE = 0.01 
-  optimizer = torch.optim.Adagrad(params=model.parameters(), lr=LEARNING_RATE)
+    LEARNING_RATE = 0.01 
+    optimizer = torch.optim.Adagrad(params=model.parameters(), lr=LEARNING_RATE)
 
-  tr_loss = 0 
-  tr_steps = 0
+    tr_loss = 0 
+    tr_steps = 0
 
-  # put model in training mode
-  model.train()
- 
+    # put model in training mode
+    model.train()
+  
 
-  correct = 0 
-  total =  0 
-  for idx, batch in enumerate(loader):
- 
-    inputs, targets = batch
- 
-    predictions = model(torch.LongTensor(inputs))
-
-    loss = loss_function(predictions, targets)
-    tr_loss += loss.item()
-
-    #print("Batch loss: ", loss.item()) # Helpful for debugging, maybe 
-
-    tr_steps += 1
+    correct = 0 
+    total =  0 
+    for idx, batch in enumerate(loader):
+  
+        inputs, targets = batch
     
-    if idx % 1000==0:
-      curr_avg_loss = tr_loss / tr_steps
-      print(f"Current average loss: {curr_avg_loss}")
+        predictions = model(torch.LongTensor(inputs))
 
-    # To compute training accuracy for this epoch 
-    correct += sum(torch.argmax(predictions, dim=1) == torch.argmax(targets, dim=1))
-    total += len(inputs)
-      
-    # Run the backward pass to update parameters 
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
+        loss = loss_function(predictions, targets)
+        tr_loss += loss.item()
+
+        #print("Batch loss: ", loss.item()) # Helpful for debugging, maybe 
+
+        tr_steps += 1
+        
+        if idx % 1000==0:
+            curr_avg_loss = tr_loss / tr_steps
+            print(f"Current average loss: {curr_avg_loss}")
+
+        # To compute training accuracy for this epoch 
+        correct += sum(torch.argmax(predictions, dim=1) == torch.argmax(targets, dim=1))
+        total += len(inputs)
+          
+        # Run the backward pass to update parameters 
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
 
-  epoch_loss = tr_loss / tr_steps
-  acc = correct / total
-  print(f"Training loss epoch: {epoch_loss},   Accuracy: {acc}")
+    epoch_loss = tr_loss / tr_steps
+    acc = correct / total
+    print(f"Training loss epoch: {epoch_loss},   Accuracy: {acc}")
 
 
 if __name__ == "__main__":
 
-    WORD_VOCAB_FILE = 'data/words.vocab'
-    POS_VOCAB_FILE = 'data/pos.vocab'
+	WORD_VOCAB_FILE = 'data/words.vocab'
+	POS_VOCAB_FILE = 'data/pos.vocab'
 
-    try:
-        word_vocab_f = open(WORD_VOCAB_FILE,'r')
-        pos_vocab_f = open(POS_VOCAB_FILE,'r')
-    except FileNotFoundError:
-        print("Could not find vocabulary files {} and {}".format(WORD_VOCAB_FILE, POS_VOCAB_FILE))
-        sys.exit(1)
+	try:
+		word_vocab_f = open(WORD_VOCAB_FILE,'r')
+		pos_vocab_f = open(POS_VOCAB_FILE,'r')
+	except FileNotFoundError:
+		print("Could not find vocabulary files {} and {}".format(WORD_VOCAB_FILE, POS_VOCAB_FILE))
+		sys.exit(1)
 
-    extractor = FeatureExtractor(word_vocab_f, pos_vocab_f)
-
-
-    model = DependencyModel(len(extractor.word_vocab), len(extractor.output_labels))
-    # model.forward(torch.randint(0, 100, [10, 6]))
-
-    dataset = DependencyDataset(sys.argv[1], sys.argv[2])
-    loader = DataLoader(dataset, batch_size = 16, shuffle = True)
-
-    print("Done loading data")
-
-    # Now train the model
-    for i in range(5): 
-      train(model, loader)
+	extractor = FeatureExtractor(word_vocab_f, pos_vocab_f)
 
 
-    torch.save(model.state_dict(), sys.argv[3]) 
+	model = DependencyModel(len(extractor.word_vocab), len(extractor.output_labels))
+	# model.forward(torch.randint(0, 100, [10, 6]))
+
+	dataset = DependencyDataset(sys.argv[1], sys.argv[2])
+	loader = DataLoader(dataset, batch_size = 16, shuffle = True)
+
+	print("Done loading data")
+
+	# Now train the model
+	for i in range(5): 
+		train(model, loader)
+
+	torch.save(model.state_dict(), sys.argv[3]) 
